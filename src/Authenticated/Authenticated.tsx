4b630f36b7ee58d4a8ffe8ee62c9cbdf016753b3
Bugsyaya <github@bugsyaya.dev>
2024-04-12 21:42:58 +0200

    Add logout

--- src/Authenticated/Authenticated.tsx
+++ src/Authenticated/Authenticated.tsx
@@ -1,12 +1,13 @@
 import { Navigation, Roles } from "@pixel-brew/bubble-craft"
 import { useContext } from "react"
-import { Navigate, Outlet, useLocation } from "react-router-dom"
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom"
 import packageJson from "../../package.json"
 import { AuthentificationContexte } from "../App.js"
 import "./authenticated.css"
 
 export const Authenticated = () => {
   const location = useLocation()
+  const navigate = useNavigate()
   const { token } = useContext(AuthentificationContexte)!
 
   if (token && location.pathname === "/")
@@ -20,7 +21,13 @@ export const Authenticated = () => {
   if (token)
     return (
       <div className="authentificated">
-        <Navigation roles={[Roles.SUPER_ADMIN, Roles.PLANING_KEEPER]} />
+        <Navigation
+          roles={[Roles.SUPER_ADMIN, Roles.PLANING_KEEPER]}
+          logout={() => {
+            localStorage.removeItem("token")
+            navigate("/login")
+          }}
+        />
         <div className="right">
           <Outlet />
           <div className="footer">
